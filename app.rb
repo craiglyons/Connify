3f9347823a8260d4ac219ed375c73cea69efbb22
Craig Lyons <craiglyons.dev@gmail.com>
2016-03-26 10:31:34 -0400

    initial commit

---
+++ app.rb
@@ -0,0 +1,30 @@
+require 'sinatra/base'
+require 'slim'
+class Connify < Sinatra::Base
+
+  get '/' do
+    slim :index
+  end
+
+  post '/' do
+    set_input
+    @result = connify(@input)
+    puts "index post - input is #{@input}, result is #{@result}"
+    slim :index
+  end
+
+  get '/:num' do
+    set_input
+    @result = connify(@input)
+    slim :index
+  end
+
+  def set_input
+    @input = params[:num].to_i
+  end
+
+  def connify(input)
+    (input.is_a? Integer) ? input * 4 : "error: please enter a number."
+  end
+end
+
